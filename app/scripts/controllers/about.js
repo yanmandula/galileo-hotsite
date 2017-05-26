fd7fbf150d46304f7eda33b4d7ee9fa9bf27fdb0
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-05-25 22:52:37 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hotsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hotsiteApp
+ */
+angular.module('hotsiteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hotsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hotsiteApp
+ */
+angular.module('hotsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
